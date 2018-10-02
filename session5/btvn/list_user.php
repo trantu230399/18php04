2cae357fb72bc5d6fe41d133af952f088be3c10d
tran tu <trantu230399@gmail.com>
2018-10-02 14:47:37 +0700

    btvn

--- session5/btvn/list_user.php
+++ session5/btvn/list_user.php
@@ -6,36 +6,36 @@
         <p>Submit<input name ="click" type ="submit"></p>
         </form>
         <?php
-       
+       include 'connec.php';
+     
+
         ?>
         <h1>List User</h1>
-     <?php
-        if(isset($_POST['click'])){
-            $serch = $_POST['serch'];
-            include 'connec.php';      
-            $name = "";
-            if($serch == ''){
-                $sql = "select * from user";
-            }else{
-                $sql = "slect * FROM user where name like '%$name%' or description like '%$name%'";
-            }
-            $result =  $conn->query($sql); 
-        }
-     ?>
+    <?php
+        $sql='';
+        $serch =$_POST['serch'];
+       if($serch==''){
+           $sql ="select * from user";
+       }else{
+           $sql = "select * from user where name like '%$serch%'";
+       }      
+    
+    $result = $conn->query($sql); 
+    if($result->num_rows > 0){
+     while($row = $result ->fetch_assoc()){
+        $id = $row['id'];
+        echo "<br>";
+         echo  $row['id'].'-'. $row['name'].'-'.$row['price'].'-'.$row['description'];
+         echo " <a href='delete.php?id=$id'>DELETE</a>";
+         echo '---';
+         echo "<a href='edit.php?id=$id'>Edit</a>";
+         echo '<br>';
+     }
+ }
+    ?>
        
     <?php
-    if($result->num_rows > 0){
-        while($row = $result ->fetch_assoc()){
-           $id = $row['id'];
-           echo "<br>";
-            echo  $row['id'].'-'. $row['name'].'-'.$row['price'].'-'.$row['description'];
-            echo " <a href='delete.php?id=$id'>DELETE</a>";
-            echo '---';
-            echo "<a href='edit.php?id=$id'>Edit</a>";
-            echo '<br>';
-        }
-    }
-    
+   
     ?>
     </body>
 </html>
