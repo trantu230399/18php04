e38cf3c0a7a6ad7920c349a1c5d0534e49f567d3
tran tu <trantu230399@gmail.com>
2018-10-12 16:12:22 +0700

    sss

--- session7/mvc bootraps/common/header.php
+++ session7/mvc bootraps/common/header.php
@@ -43,7 +43,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>TVAT</b></span>
     </a>
 
     <!-- Header Navbar: style can be found in header.less -->
@@ -59,7 +59,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="images/tvat.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Alexander Pierce</span>
+              <span class="hidden-xs"> Trần Võ Anh Tú</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
--- session7/mvc bootraps/common/sidebar.php
+++ session7/mvc bootraps/common/sidebar.php
@@ -7,7 +7,7 @@
           <img src="images/tvat.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Trần Võ Anh Tú</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
